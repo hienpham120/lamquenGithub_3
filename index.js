63cbdefa514e938dfc628f6ef9d322b34bcf88fc
Hien Pham <phamthehien120@gmail.com>
2021-06-25 10:55:43 -0700

    'buoi1commit'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.log('Làm bài tập');
+
 function Main(){
     console.log('doneBaiTapBuoi1');
-    console.log('doneBaiTapBuoi2');
-    console.log('doneBaiTapBuoi3');
+    // console.log('doneBaiTapBuoi2');
+    // console.log('doneBaiTapBuoi3');
 }
